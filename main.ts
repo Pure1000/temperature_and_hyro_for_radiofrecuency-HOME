922b8f119bee4e66050873c4179dff432da17776
Pure1000 <purebionest@gmail.com>
2025-07-10 22:34:21 +0200

    Agree on configuration functions

--- main.ts
+++ main.ts
@@ -1,6 +1,22 @@
-function Configures () {
+function Microturtle_configuration () {
 	
 }
+function Configures () {
+    Configure_radio_frequency()
+    Serial_configuration()
+    Datalogger_configuration()
+    Microturtle_configuration()
+}
+function Configure_radio_frequency () {
+	
+}
+function Serial_configuration () {
+	
+}
+function Datalogger_configuration () {
+	
+}
+Configures()
 basic.forever(function () {
 	
 })
