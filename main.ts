19b24adaaa7b96cf6dcc89775ef2c81d765ce691
Pure1000 <purebionest@gmail.com>
2025-07-10 22:55:26 +0200

    Udapte

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 function Microturtle_configuration () {
-	
+    turtle.pen(TurtlePenMode.Up)
+    turtle.setBrightness(255)
+    turtle.home()
+    turtle.setSpeed(25.5)
+    turtle.setPosition(0, 0)
 }
 function Configures () {
     Configure_radio_frequency()
@@ -8,15 +12,77 @@ function Configures () {
     Microturtle_configuration()
 }
 function Configure_radio_frequency () {
-	
+    radio.setGroup(1e+99)
+    radio.setTransmitPower(17.4)
+    radio.setTransmitSerialNumber(true)
+    radio.setFrequencyBand(17.4)
+    radio.raiseEvent(
+    EventBusSource.MICROBIT_ID_RADIO,
+    EventBusValue.MES_ALERT_EVT_VIBRATE
+    )
 }
 function Serial_configuration () {
-	
+    serial.redirect(
+    SerialPin.USB_TX,
+    SerialPin.USB_RX,
+    BaudRate.BaudRate115200
+    )
+    serial.setTxBufferSize(32)
+    serial.setRxBufferSize(32)
+    serial.writeBuffer(serial.readBuffer(0))
+    serial.setWriteLinePadding(0)
+    serial.setBaudRate(BaudRate.BaudRate115200)
+    serial.writeLine("º - º - º - º - º - º - º - º - º - º - º - º - º")
+    serial.redirectToUSB()
 }
 function Datalogger_configuration () {
-	
+    datalogger.includeTimestamp(FlashLogTimeStampFormat.Hours)
+    datalogger.setColumnTitles(
+    "time",
+    "Hyro",
+    "ºC"
+    )
 }
 Configures()
 basic.forever(function () {
-	
+    turtle.forward(1)
+    turtle.setPosition(1, 0)
+    turtle.forward(1)
+    turtle.setPosition(2, 0)
+    turtle.forward(1)
+    turtle.setPosition(3, 0)
+    turtle.forward(1)
+    turtle.setPosition(4, 0)
+    turtle.forward(1)
+    turtle.setPosition(0, 1)
+    turtle.forward(1)
+    turtle.setPosition(1, 1)
+    turtle.forward(1)
+    turtle.setPosition(2, 1)
+    turtle.forward(1)
+    turtle.setPosition(4, 1)
+    turtle.forward(1)
+    turtle.setPosition(1, 2)
+    turtle.forward(1)
+    turtle.setPosition(2, 2)
+    turtle.forward(1)
+    turtle.setPosition(3, 2)
+    turtle.forward(1)
+    turtle.setPosition(4, 2)
+    turtle.forward(1)
+    turtle.setPosition(0, 3)
+    turtle.forward(1)
+    turtle.setPosition(1, 3)
+    turtle.forward(1)
+    turtle.setPosition(2, 3)
+    turtle.forward(1)
+    turtle.setPosition(4, 3)
+    turtle.forward(1)
+    turtle.setPosition(1, 4)
+    turtle.forward(1)
+    turtle.setPosition(2, 4)
+    turtle.forward(1)
+    turtle.setPosition(3, 4)
+    turtle.forward(1)
+    turtle.setPosition(4, 4)
 })
